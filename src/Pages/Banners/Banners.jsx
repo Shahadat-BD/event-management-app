a335c7af98956f2d044b6fb07d62e4b44c2ab28a
Shahadat-BD <shahadatstudent34@gmail.com>
2023-10-07 00:27:54 +0000

    banner section added

--- src/Pages/Banners/Banners.jsx
+++ src/Pages/Banners/Banners.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import './Banner.css'
 const Banners = () => {
     return (
-        <div>
-            <h1>This is banner section</h1>
+        <div className='banner-added'>
+            <div className='flex justify-center items-center lg:h-[600px] h-[350px]'>
+                <h1 className='lg:text-8xl md:text-2xl text-3xl text-center text-white font-semibold'>The Best <span  className='text-orange-500'>Social Events</span> <br/>
+                In the world</h1>
+            </div>
         </div>
     );
 };
